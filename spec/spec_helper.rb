96e07bd4a6ef22556352eeac633a148600725a8d
David Asabina <david@supr.nu>
2014-09-08 22:36:02 +0200

    Out with the Minitest ANSI relics

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,7 @@ require 'bundler/setup'
 Bundler.setup
 
 require 'minitest'
-require 'minitest/ansi'
 require 'minitest/autorun'
 require 'minitest/benchmark'
-
-#MiniTest::ANSI.use!
 
 require 'shufflr'
